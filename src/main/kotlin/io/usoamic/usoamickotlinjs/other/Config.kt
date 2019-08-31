a299e14d8017dd476153f9d664bea800b59c254f
primys <a2uso@tuta.io>
2019-08-31 09:43:38 -0700

    Add Config file

---
+++ src/main/kotlin/io/usoamic/usoamickotlinjs/other/Config.kt
@@ -0,0 +1,9 @@
+package io.usoamic.usoamickotlinjs.other
+
+class Config {
+    companion object {
+        const val NODE: String = "https://rinkeby.infura.io:443"
+        const val ACCOUNT_FILENAME: String = "account.json"
+        const val CONTRACT_ADDRESS: String = "0xb894d4329329b6150258c0048f6dfb41257359f5"
+    }
+}
