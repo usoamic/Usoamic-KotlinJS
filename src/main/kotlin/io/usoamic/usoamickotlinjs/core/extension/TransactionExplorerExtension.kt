4871e69c97eac70d6c20c6973e560360ffcf7fb0
primys <a2uso@tuta.io>
2019-09-08 12:00:27 -0700

    Remove nullable from lastId and add hasUpdate param to callback

--- src/main/kotlin/io/usoamic/usoamickotlinjs/core/extension/TransactionExplorerExtension.kt
+++ src/main/kotlin/io/usoamic/usoamickotlinjs/core/extension/TransactionExplorerExtension.kt
@@ -5,30 +5,51 @@ import io.usoamic.usoamickotlinjs.model.Transaction
 import io.usoamic.web3kt.core.contract.model.CallOption
 import kotlin.math.min
 
-private fun TransactionExplorer.iterateTransactions(option: CallOption, list: MutableList<Transaction>, owner: String, id: Long, lastId: Long, callback: (list: MutableList<Transaction>) -> Unit) {
-    getTransactionByAddress(owner, id.toString()).call(option).then { tx ->
-        list.add(tx)
-        val nextId = (id + 1)
-        if(nextId < lastId) {
-            iterateTransactions(option, list, owner, nextId, lastId, callback)
+private fun TransactionExplorer.iterateTransactions(
+    option: CallOption,
+    list: MutableList<Transaction>,
+    owner: String,
+    id: Long,
+    lastId: Long,
+    callback: (list: MutableList<Transaction>, t: Throwable?, needLoad: Boolean) -> Unit
+) {
+    getTransactionByAddress(owner, id.toString()).call(option)
+        .then { tx ->
+            list.add(tx)
+            val nextId = (id + 1)
+            if (nextId < lastId) {
+                iterateTransactions(option, list, owner, nextId, lastId, callback)
+            } else {
+                callback(list, null, true)
+            }
         }
-        else {
-            callback(list)
+        .catch {
+            callback(mutableListOf(), it, false)
         }
-    }
 }
 
-fun TransactionExplorer.getTransactionsByAddress(owner: String, lastId: Long?, callback: (list: MutableList<Transaction>) -> Unit) {
+fun TransactionExplorer.getTransactionsByAddress(
+    owner: String,
+    maxTx: Long,
+    loadedLastId: Long,
+    callback: (list: MutableList<Transaction>, t: Throwable?, hasUpdate: Boolean) -> Unit
+) {
     val option = CallOption(from = owner)
 
-    getNumberOfTransactionsByAddress(owner).call(option).then {
-        val numberOfTx = it.toLong()
-        val factLastId = if(lastId == null) numberOfTx else min((lastId + 1), numberOfTx)
-        if(factLastId > 0) {
-            iterateTransactions(option, mutableListOf(), owner, 0, factLastId, callback)
+    getNumberOfTransactionsByAddress(owner).call(option)
+        .then {
+            val factLastId = min(maxTx, it.toLong())
+            if(loadedLastId == factLastId) {
+                callback(mutableListOf(), null, false)
+                return@then
+            }
+            if (factLastId > 0) {
+                iterateTransactions(option, mutableListOf(), owner, 0, factLastId, callback)
+            } else {
+                callback(mutableListOf(), null, true)
+            }
         }
-        else {
-            callback(mutableListOf())
+        .catch {
+            callback(mutableListOf(), it, false)
         }
-    }
 }
