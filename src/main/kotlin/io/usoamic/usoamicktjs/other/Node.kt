0b70817dab024383722df4d5a558b872ff5ce010
primys <a2uso@tuta.io>
2019-11-03 08:57:31 -0800

    Add Project ID of Infura

--- src/main/kotlin/io/usoamic/usoamicktjs/other/Node.kt
+++ src/main/kotlin/io/usoamic/usoamicktjs/other/Node.kt
@@ -5,11 +5,12 @@ import io.usoamic.usoamicktjs.exception.NodeProviderNotFound
 import io.usoamic.usoamicktjs.enumcls.NodeProvider
 
 object Node {
+    private const val PROJECT_ID = "fa8e5195184149f79e644292d2b31707"
     fun by(networkType: NetworkType, nodeProvider: NodeProvider): String {
         return when(nodeProvider) {
             NodeProvider.INFURA -> when(networkType) {
-                NetworkType.MAINNET -> "https://mainnet.infura.io:443"
-                NetworkType.TESTNET -> "https://rinkeby.infura.io:443"
+                NetworkType.MAINNET -> "https://mainnet.infura.io:443/v3/${PROJECT_ID}"
+                NetworkType.TESTNET -> "https://rinkeby.infura.io:443/v3/${PROJECT_ID}"
             }
             NodeProvider.MYETHERWALLET -> when(networkType) {
                 NetworkType.MAINNET -> "https://api.myetherwallet.com:443/eth"
